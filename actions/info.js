7926e60a85c73fcfeede3a6b351d395ea7414cdb
Paul Panserrieu <paul.panserrieu@gmail.com>
2014-05-08 22:22:54 +0200

    ...

--- actions/info.js
+++ actions/info.js
@@ -3,6 +3,7 @@ module.exports = {
     help : 'current project information',
     action : function (options) {
         // get some info about the current project
+        // which sdk are installed on the current host
     },
     options : [{
         name : 'verbose',
