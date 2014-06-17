71d02ea589e3a636dff6f80b992517b5b26cb0db
Paul Panserrieu <paul.panserrieu@gmail.com>
2014-06-17 22:58:59 +0200

    add available platforms in settings

--- actions/create/questions/platforms.js
+++ actions/create/questions/platforms.js
@@ -1,13 +1,11 @@
+var settings = require('../../../lib/settings');
+
 module.exports = {
-    type:'checkbox',
-    name:'platforms',
+    type : 'checkbox',
+    name : 'platforms',
     validate : function (val) { return val.length > 0 || "One platform is mandatory!"; },
-    choices:[{
-        name:'iOS',
-        value:'ios'
-    },{
-        name:'android',
-        value:'android'
-    }],
-    message:'What are the supported platform of your project?'
+    choices : settings.platforms.map(function (setting) {
+        return { name : setting, value : setting };
+    }),
+    message : 'What are the supported platform of your project?'
 };
