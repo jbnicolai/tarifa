a6b09d9a13112b5a57da482889717c3236e17a30
Paul Panserrieu <paul.panserrieu@gmail.com>
2014-06-17 22:59:17 +0200

    wip prepare

--- actions/prepare/index.js
+++ actions/prepare/index.js
@@ -5,17 +5,6 @@ var Q = require('q'),
     path = require('path'),
     fs = require('fs');
 
-/*
- * First draft
- * tarifa prepare configuration
- * tarifa prepare ios:staging
- *
- * TODO  
- *  tarifa prepare -> te demande ce que tu veux preparer...
- * multi call 
- * ex tarifa prepare ios:staging android:staging android:production
- */
-
 module.exports = function (argv) {
     if(argsHelper.matchSingleOptions(argv, 'h', 'help')) {
         console.log(fs.readFileSync(path.join(__dirname, 'usage.txt'), 'utf-8'));
@@ -24,7 +13,7 @@ module.exports = function (argv) {
 
     if(argsHelper.matchSingleOptions(argv, 'V', 'verbose')) {
         verbose = true;
-    } else if(argv._.length != 1) {
+    } else if(argv._.length != 1 && argv._.length != 2) {
         console.log(fs.readFileSync(path.join(__dirname, 'usage.txt'), 'utf-8'));
         return Q.resolve();
     }
@@ -34,11 +23,24 @@ module.exports = function (argv) {
     if(!fs.existsSync(path.join(process.cwd(), 'tarifa.json')))
         return Q.reject(new Error('no tarifa.json file available in the current working directory!'));
 
-    // extract platform and configuration
+    // check that we have the wanted platform and configuration
     var localSettings = require(tarifaFilePath);
     var tmp = argv._[0].split(':');
     var platform = tmp[0];
-    var config = tmp[1];
+
+    if(settings.platforms.indexOf(platform) < 0) {
+        return Q.reject(new Error('platform not availble!'));
+    }
+
+    if (localSettings.platforms.indexOf(platform) < 0) {
+        return Q.reject(new Error('platform not described in tarifa.json'));
+    }
+
+    var config = tmp[1] || 'default';
+
+    if(!localSettings.configurations[platform][config]) {
+        return Q.reject(new Error('configuration ' + platform + ' not described in tarifa.json'));
+    }
 
     // link app www to project output
     var defer = Q.defer();
@@ -53,11 +55,8 @@ module.exports = function (argv) {
         });
     });
 
-    // build www project whith it's own build module
     var builder = require(path.join(process.cwd(), settings.build));
     return defer.promise.then(function () {
         return builder(platform, localSettings.configurations[platform][config]);
     });
-
-    // we may need to unlink...
 };
