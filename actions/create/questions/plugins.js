f49ba6796da898e49a837efc746d26f586205cf8
Paul Panserrieu <paul.panserrieu@gmail.com>
2014-07-01 22:48:30 +0200

    mv plugins json config to lib/

--- actions/create/questions/plugins.js
+++ actions/create/questions/plugins.js
@@ -4,6 +4,6 @@ var fs = require('fs'),
 module.exports = {
     type:'checkbox',
     name:'plugins',
-    choices:JSON.parse(fs.readFileSync(path.join(__dirname, '../../../conf/plugins.json'))),
+    choices:JSON.parse(fs.readFileSync(path.join(__dirname, '../../../lib/plugins.json'))),
     message:'Which plugins do you want to install right now?'
 };
