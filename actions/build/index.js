f617cec1a34a80378ffe079d8742c2bb2bfa5d4a
Paul Panserrieu <paul.panserrieu@gmail.com>
2014-07-01 17:34:32 +0200

    start refactoring build cmd

--- actions/build/index.js
+++ actions/build/index.js
@@ -1,55 +1,38 @@
 var Q = require('q'),
+    chalk = require('chalk'),
     cordova = require('cordova'),
     argsHelper = require('../../lib/args'),
     settings = require('../../lib/settings'),
+    tarifaFile = require('../../lib/tarifa-file'),
     path = require('path'),
     fs = require('fs'),
     prepareAction = require('../prepare');
 
 var build = function (platform, config, verbose) {
-    // check if we have a tarifa.json file on the current dir
-    var tarifaFilePath = path.join(process.cwd(), 'tarifa.json');
-    if(!fs.existsSync(path.join(process.cwd(), 'tarifa.json')))
-        return Q.reject(new Error('No tarifa.json file available in the current working directory!'));
+    var cwd = process.cwd();
+    var tarifaFilePath = path.join(cwd, 'tarifa.json');
 
-    // check that we have the wanted platform and configuration
-    var localSettings = require(tarifaFilePath);
+    return tarifaFile.parseFromFile(tarifaFilePath, platform, config).then(function (localSettings) {
+        var defer = Q.defer();
+        var localConf = localSettings.configurations[platform][config];
+        var mode = (platform === 'android' && (localConf.keystore_path && localConf.keystore_alias)) ? '--release' : null;
 
-    if(settings.platforms.indexOf(platform) < 0) {
-        return Q.reject(new Error('platform not availble!'));
-    }
-
-    if (localSettings.platforms.indexOf(platform) < 0) {
-        return Q.reject(new Error('platform not described in tarifa.json'));
-    }
-
-    if(!localSettings.configurations[platform][config]) {
-        return Q.reject(new Error('configuration ' + platform + ' not described in tarifa.json'));
-    }
-
-    var defer = Q.defer();
-    var cordova_path = path.join(process.cwd(), settings.cordovaAppPath);
-    var localConf = localSettings.configurations[platform][config];
-    // TODO for ios
-    var mode = (platform === 'android' && (localConf.keystore_path && localConf.keystore_alias)) ? '--release' : null;
-
-    if(verbose) console.log('start to build the www project');
-    prepareAction.prepare(platform, config, verbose).then(function () {
-        process.chdir(cordova_path);
-        if(verbose) console.log('start cordova build');
-
-        cordova.build({
-            verbose: verbose,
-            platforms: [ platform ],
-            options: mode ? [ mode ] : []
-        }, function (err, result) {
-            process.chdir('..');
-            if(err) defer.reject(err);
-            defer.resolve();
+        if(verbose) console.log(chalk.green('✔') + ' start to build the www project');
+        return prepareAction.prepare(platform, config, verbose).then(function () {
+            process.chdir(path.join(cwd, settings.cordovaAppPath));
+            if(verbose) console.log(chalk.green('✔') + ' start cordova build');
+            cordova.build({
+                verbose: verbose,
+                platforms: [ platform ],
+                options: mode ? [ mode ] : []
+            }, function (err, result) {
+                process.chdir(cwd);
+                if(err) defer.reject(err);
+                defer.resolve();
+            });
+            return defer.promise;
         });
     });
-
-    return defer.promise;
 };
 
 var action = function (argv) {
@@ -66,9 +49,9 @@ var action = function (argv) {
         return Q.resolve();
     }
 
-    var tmp = argv._[0].split(':');
-    return build(tmp[0], tmp[1] || 'default', verbose);
+    return build(argv._[0], argv._[1] || 'default', verbose);
 }
 
 action.build = build;
+
 module.exports = action;
