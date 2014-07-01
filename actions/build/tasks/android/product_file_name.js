b1cbea992f9e5e29eb31da41642e44f17dc26254
Paul Panserrieu <paul.panserrieu@gmail.com>
2014-07-01 22:35:00 +0200

    [build cmd] change product file name for android

--- actions/build/tasks/android/product_file_name.js
+++ actions/build/tasks/android/product_file_name.js
@@ -1,9 +1,17 @@
 var Q = require('q'),
-    chalk = require('chalk');
+    libxmljs = require('libxmljs'),
+    chalk = require('chalk'),
+    path = require('path'),
+    fs = require('fs'),
+    settings = require('../../../../lib/settings');
 
 module.exports = function (msg) {
     var product_name = msg.settings.configurations.android[msg.config]['product_file_name'];
-    // TODO swap the project name in the build.xml of the android project
+    var build_xml_file_path = path.join(process.cwd(), settings.cordovaAppPath, 'platforms', 'android', 'build.xml');
+
+    var doc = libxmljs.parseXml(fs.readFileSync(build_xml_file_path));
+    fs.writeFileSync(build_xml_file_path, doc.root().attr('name', product_name));
+
     if(msg.verbose)
         console.log(chalk.green('✔') + ' change product_file_name to ' + product_name);
     return Q.resolve(msg);
