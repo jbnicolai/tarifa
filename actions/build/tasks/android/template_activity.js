a8921521e55d7b478d998b2cb2333bbed671f581
Paul Panserrieu <paul.panserrieu@gmail.com>
2014-07-02 16:57:23 +0200

    [build cmd] wip template cordova activity

--- actions/build/tasks/android/template_activity.js
+++ actions/build/tasks/android/template_activity.js
@@ -1,6 +1,7 @@
 var Q = require('q');
 
 module.exports = function (msg) {
-    // TODO
+    // TODO rm old android package with java activity
+    // and generate a new one if the configuration id is not equal the default one
     return Q.resolve(msg);
 };
