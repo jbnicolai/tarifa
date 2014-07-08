7513e9d74a25ec8745f573dc54ef9f83b691ea30
Paul Panserrieu <paul.panserrieu@gmail.com>
2014-07-08 16:22:04 +0200

    wip on the index.html of the www template project

--- template/project/src/app.js
+++ template/project/src/app.js
@@ -4,7 +4,8 @@ var qstart = require('qstart'),
 
 qstart.then(function () {
     var p = window.document.querySelector('p');
-    Zanimo(p, 'transform', 'rotate(180deg)', 500, 'ease-in-out').then(function (el) {
+    Zanimo(p, 'transform', 'scale(0.5)', 500, 'ease-in-out').then(function (el) {
         el.innerHTML = 'welcome to ' + configuration.app_label + ' with id ' + configuration.id;
-    });
+        return el;
+    }).then(Zanimo.f('transform', 'scale(1)', 500, 'ease-in-out'));
 });
