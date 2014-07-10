64849e7ecf3705cf9f20c561e1f3317c2103cf63
Paul Panserrieu <paul.panserrieu@gmail.com>
2014-07-10 16:19:41 +0200

    now, when changing the android activity, we rm also the top level package folder
    it's a convention, we don't ever share an android activity top level package to any other source code, that means, `org` can not be choosen has a top level package name for the app...

--- actions/build/tasks/android/change_template_activity.js
+++ actions/build/tasks/android/change_template_activity.js
@@ -10,23 +10,19 @@ var Q = require('q'),
 module.exports = function (msg) {
     var defer = Q.defer();
     var cwd = process.cwd();
-    var name = msg.settings.configurations.android[msg.config]['name'];
+    var conf = msg.settings.configurations.android;
+    var name = conf[msg.config]['name'] || conf['default']['name'] || conf['name'];
     var id = msg.settings.configurations.android[msg.config]['id'] || msg.settings.id;
     var srcPath = path.join(cwd, settings.cordovaAppPath, '/platforms/android/src/');
     var finder = find(path.join(cwd, settings.cordovaAppPath, '/platforms/android/src/'));
     var javaActivityTmpl = fs.readFileSync(path.join(__dirname, 'activity.java.tmpl'), 'utf-8');
     var androidManifestXmlPath = path.join(cwd, settings.cordovaAppPath, 'platforms/android/AndroidManifest.xml');
 
-    finder.on('file', function (file, stat) {
-        if(path.basename(file) == (name + '.java')) {
-            rimraf(file, function (err) {
-                if(err) defer.reject("unable to remove file " + file);
-            });
-        }
-    });
-
-    finder.on('end', function (file, stat) {
-        var asbPath = path.join(srcPath, id.replace(/\./g, '/'));
+    // the top level package can not be used for anythings else as the main activity
+    var file = path.join(srcPath, id.split('.')[0]);
+    var asbPath = path.join(srcPath, id.replace(/\./g, '/'));
+    rimraf(file, function (err) {
+        if(err) defer.reject("unable to remove file " + file);
         mkdirp(asbPath, function (err) {
             if (err) {
                 defer.reject("unable to create package " + asbPath);
@@ -35,7 +31,9 @@ module.exports = function (msg) {
                 var activity = javaActivityTmpl.replace(/\$PACKAGE_NAME/, id).replace(/\$APP_NAME/, name);
                 fs.writeFileSync(path.join(asbPath, name + '.java'), activity);
                 var androidManifestXml = libxmljs.parseXml(fs.readFileSync(androidManifestXmlPath));
-                fs.writeFileSync(androidManifestXmlPath, androidManifestXml.root().attr('package', id));
+                androidManifestXml.root().attr('package', id);
+                androidManifestXml.get('/manifest/application/activity').attr('android:name', name);
+                fs.writeFileSync(androidManifestXmlPath, androidManifestXml.root());
                 defer.resolve(msg);
             }
         });
