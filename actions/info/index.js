bf0a15738d0934150d20174136265a3a2c40efc7
Paul Panserrieu <paul.panserrieu@gmail.com>
2014-07-12 21:53:18 +0200

    refactor getAndroidDevices, return an array of serial numbers

--- actions/info/index.js
+++ actions/info/index.js
@@ -5,15 +5,13 @@ var Q = require('q'),
     pkg = require('../../package.json');
 
 module.exports = function (argv) {
-
         console.log(chalk.green('node version:               ') + process.versions.node);
         console.log(chalk.green('cordova version:            ') + pkg.dependencies.cordova);
 
     return devices.ios().then(function (devices) {
-        console.log(chalk.green('connected iOS devices:      ') +  devices.join('\n'));
+        console.log(chalk.green('connected iOS devices:      \n\t') +  devices.join('\n\t'));
     }).then(devices.android).then(function (devices) {
-        var out = devices.replace('List of devices attached', '').replace('\t', ' ').replace('\n', '');
-        console.log(chalk.green('connected Android devices: ') +  out);
+        console.log(chalk.green('connected Android devices:') +  devices.join('\n\t'));
     });
 
     // check installed xcode version if available
