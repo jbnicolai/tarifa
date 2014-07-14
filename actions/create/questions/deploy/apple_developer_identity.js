04faefc3825a7dd20a890b47fa288817a27b7530
Paul Panserrieu <paul.panserrieu@gmail.com>
2014-07-14 11:36:45 +0200

    add default apple developer identity and add it to the choice

--- actions/create/questions/deploy/apple_developer_identity.js
+++ actions/create/questions/deploy/apple_developer_identity.js
@@ -1,16 +1,19 @@
 var Q = require('q'),
     chalk = require('chalk'),
     exec = require('child_process').exec,
+    settings = require('../../../../lib/settings'),
     getAppleDeveloperIdentities = require('../../../../lib/apple_developer_identity');
 
 var question = function (verbose) {
     return getAppleDeveloperIdentities(verbose).then(function (identities) {
+        var ids = identities;
+        ids.push(settings.default_apple_developer_identity);
         return {
             dependency: 'ios',
             type:'list',
             name:'apple_developer_identity',
-            choices: identities,
-            default: 0,
+            choices: ids,
+            default: settings.default_apple_developer_identity,
             message:'Which Developer Identity do you take to build the adhoc distribution?'
         };
     });
