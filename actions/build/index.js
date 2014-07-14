a228a7a63d0b6e52a2d25a6bac037b14f32da37d
Paul Panserrieu <paul.panserrieu@gmail.com>
2014-07-14 16:37:59 +0200

    add release mode also for iOS when we have the right config

--- actions/build/index.js
+++ actions/build/index.js
@@ -88,14 +88,24 @@ var runTasks = function (type, platform, config, localSettings, verbose) {
     };
 };
 
+var setMode = function (platform, config, localSettings) {
+    var mode = null,
+        localConf = localSettings.configurations[platform][config];
+
+    if (platform === 'android' && localConf.keystore_path && localConf.keystore_alias) {
+        mode = '--release';
+    }
+    if(platform === 'ios' && localConf.apple_developer_identity && localConf.provisioning_profile) {
+        mode = '--release'
+    }
+};
+
 var build = function (platform, config, verbose) {
     var cwd = process.cwd();
     var tarifaFilePath = path.join(cwd, 'tarifa.json');
 
     return tarifaFile.parseFromFile(tarifaFilePath, platform, config).then(function (localSettings) {
-        var localConf = localSettings.configurations[platform][config];
-        // release mode if we have a valid keystore and keyalias
-        localSettings.mode = (platform === 'android' && (localConf.keystore_path && localConf.keystore_alias)) ? '--release' : null;
+        localSettings.mode = setMode(platform, config, localSettings);
 
         if(verbose) console.log(chalk.green('✔') + ' start to build the www project');
 
