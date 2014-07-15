f655bec62d7872cd5e7d3a256960d130a7ad7c22
Vincent Velociter <vincent.velociter@gmail.com>
2014-07-15 15:05:22 +0200

    Extract platforms management to lib

--- actions/create/tasks/platforms.js
+++ actions/create/tasks/platforms.js
@@ -2,40 +2,14 @@
  * add cordova platforms task
  */
 
-var path = require('path'),
-    cordova_platform_add = require('cordova/src/platform').add,
-    cordova_util = require('cordova/src/util'),
-    cordova_hooker = require('cordova/src/hooker'),
-    Q = require('q'),
-    chalk = require('chalk'),
-    settings = require('../../../lib/settings');
+var platformsTool = require('../../../lib/platforms');
 
 module.exports = function (response) {
-    var cordova_path = path.join(response.path, settings.cordovaAppPath),
-        cwd = process.cwd();
-
-    process.chdir(cordova_path);
-
-    var projectRoot = cordova_util.cdProjectRoot(),
-        hooks = new cordova_hooker(projectRoot),
-        opts = {
-            platforms:response.platforms,
-            // android output too much stuff, ignore it for now!
-            spawnoutput: {
-                stdio: 'ignore'
-            }
-        };
-
     var platforms = response.platforms.filter(function (platform) { return platform != 'web'; });
 
-    return cordova_platform_add(hooks, projectRoot, platforms, opts).then(function (err) {
-        process.chdir(cwd);
-        if(err) return Q.reject(err);
-        if (response.options.verbose) {
-            response.platforms.forEach(function (target) {
-                console.log(chalk.green('✔') + ' cordova platform ' + target + ' added');
-            });
-        }
+    process.chdir(response.path);
+
+    return platformsTool.add(platforms, response.options.verbose).then(function() {
         return response;
     });
 };
