69d6d58f8c0089f0fad517b7c0998056ac320cb9
Paul Panserrieu <paul.panserrieu@gmail.com>
2014-07-16 23:43:56 +0200

    fix platform task in tarifa create
    need to go back to root folder

--- actions/create/tasks/platforms.js
+++ actions/create/tasks/platforms.js
@@ -6,10 +6,12 @@ var platformsLib = require('../../../lib/platforms');
 
 module.exports = function (response) {
     var platforms = response.platforms.filter(function (platform) { return platform != 'web'; });
+    var cwd = process.cwd();
 
     process.chdir(response.path);
 
     return platformsLib.add(platforms, response.options.verbose).then(function() {
+        process.chdir(cwd);
         return response;
     });
 };
