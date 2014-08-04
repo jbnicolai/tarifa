7756fee4b9a0644d3e7736489c801033c59f6de8
Paul Panserrieu <paul.panserrieu@gmail.com>
2014-08-04 17:36:22 +0200

    [tarifa create] add validation in project name question

--- actions/create/questions/name.js
+++ actions/create/questions/name.js
@@ -1,12 +1,10 @@
-// TODO validation
-// name is the name of the project
-// for the android platform, cordova will use it as the class name
-// of the main application activity
-// -> it muse be a valid java class name
+var validJavaIdentifier = require('valid-java-identifier');
 
 module.exports = {
     type:'input',
     name:'name',
-    validate : function (answer) { return answer.length > 0; },
+    validate : function (response) {
+        return validJavaIdentifier(response) || "It must me a valid java identifier!";
+    },
     message:'What\'s the name of your project?'
 };
