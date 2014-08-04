668f1b550a3b01bce788f7bd5a7374a705652b62
Paul Panserrieu <paul.panserrieu@gmail.com>
2014-08-04 22:15:30 +0200

    fix platform choice in tarifa create
    show only platforms which are supported on the host os.

--- actions/create/questions/platforms.js
+++ actions/create/questions/platforms.js
@@ -1,11 +1,17 @@
-var settings = require('../../../lib/settings');
+var hostOS = require('os').platform(),
+    settings = require('../../../lib/settings');
+
+function availablePlatforms() {
+    return settings.platforms.filter(function (platform) {
+        return settings.os_platforms[platform].indexOf(hostOS) > -1;
+    });
+}
 
 module.exports = {
     type : 'checkbox',
     name : 'platforms',
     validate : function (val) { return val.length > 1 || "More than one platform is mandatory!"; },
-  	// FIXME, we also need to filter on the platforms which are working in the given platform os host
-    choices : settings.platforms.map(function (setting) {
+    choices : availablePlatforms().map(function (setting) {
         return { name : setting, value : setting };
     }),
     message : 'What are the supported platform of your project?'
