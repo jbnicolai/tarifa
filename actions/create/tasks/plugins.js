b7fe7ad159b8f76eb6949d6d1a8b8040784ddb2f
Paul Panserrieu <paul.panserrieu@gmail.com>
2014-08-04 22:07:59 +0200

    starting lib/cordova/plugins

--- actions/create/tasks/plugins.js
+++ actions/create/tasks/plugins.js
@@ -3,28 +3,16 @@
  */
 
 var path = require('path'),
-    cordova = require('cordova'),
-    Q = require('q'),
     chalk = require('chalk'),
+    plugins = require('../../../lib/cordova/plugins'),
     settings = require('../../../lib/settings');
 
 module.exports = function (response) {
-    var cordova_path = path.join(response.path, settings.cordovaAppPath),
-        cwd = process.cwd(),
-        defer = Q.defer();
-
     if(response.plugins.length === 0 ) return Q.resolve(response);
 
-    process.chdir(cordova_path);
-
-    cordova.plugin('add', response.plugins, function (err) {
-        process.chdir(cwd);
-        if(err) {
-            defer.reject(err);
-            return;
-        }
-        if (response.options.verbose) console.log(chalk.green('✔') + ' cordova plugins added');
-        defer.resolve(response);
+    return plugins.add(response.path, response.plugins).then(function () {
+        if (response.options.verbose)
+            console.log(chalk.green('✔') + ' cordova plugins added');
+        return response;
     });
-    return defer.promise;
 };
