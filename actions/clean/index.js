12576677ebcec8ff8459bc2c10ea77a1ad90f928
Paul Panserrieu <paul.panserrieu@gmail.com>
2014-08-07 13:55:22 +0200

    clean tarifa clean

--- actions/clean/index.js
+++ actions/clean/index.js
@@ -1,39 +1,38 @@
 var Q = require('q'),
     spinner = require("char-spinner"),
     argsHelper = require('../../lib/helper/args'),
+    pathHelper = require('../../lib/helper/path'),
     print = require('../../lib/helper/print'),
     tarifaFile = require('../../lib/tarifa-file'),
     isAvailableOnHost = require('../../lib/cordova/platforms').isAvailableOnHost,
     cordovaClean = require('../../lib/cordova/clean'),
     path = require('path'),
-    fs = require('fs');
+    fs = require('q-io/fs');
 
 var clean = function (platform, verbose) {
-    var tarifaFilePath = path.join(process.cwd(), 'tarifa.json');
     spinner();
 
-    return tarifaFile.parseConfig(tarifaFilePath).then(function (localSettings) {
-        if(!isAvailableOnHost(platform)) return Q.reject('platform not available in host!');
-        if(platform && localSettings.platforms.indexOf(platform) < 0) return Q.reject('platform not available in project!');
+    return tarifaFile.parseConfig(pathHelper.current()).then(function (localSettings) {
+        if(!isAvailableOnHost(platform))
+            return Q.reject('platform not available in host!');
+        if(platform && localSettings.platforms.indexOf(platform) < 0)
+            return Q.reject('platform not available in project!');
         return cordovaClean(platform ? [platform] : localSettings.platforms, verbose);
     });
 };
 
 var action = function (argv) {
-    var verbose = false;
-    if(argsHelper.matchSingleOption(argv, 'h', 'help')) {
-        print(fs.readFileSync(path.join(__dirname, 'usage.txt'), 'utf-8'));
-        return Q.resolve();
-    }
+    var verbose = false,
+        helpPath = path.join(__dirname, 'usage.txt');
 
-    if(argsHelper.matchSingleOption(argv, 'V', 'verbose')) {
-        verbose = true;
-    } else if(argv._.length > 1) {
-        print(fs.readFileSync(path.join(__dirname, 'usage.txt'), 'utf-8'));
-        return Q.resolve();
+    if(argsHelper.matchArgumentsCount(argv, [1])
+            && argsHelper.checkValidOptions(argv, ['V', 'verbose'])) {
+        if(argsHelper.matchOption(argv, 'V', 'verbose')) {
+            verbose = true;
+        }
+        return clean(argv._[0], verbose);
     }
-
-    return clean(argv._[0], verbose);
+    return fs.read(helpPath).then(print);
 };
 
 action.clean = clean;
