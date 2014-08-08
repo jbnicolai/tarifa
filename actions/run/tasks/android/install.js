798e363b3a4491d1da33d1cb28cae7984af27bae
Paul Panserrieu <paul.panserrieu@gmail.com>
2014-08-08 10:21:04 +0200

    use util.format instead of string concatenation

--- actions/run/tasks/android/install.js
+++ actions/run/tasks/android/install.js
@@ -1,17 +1,23 @@
 var Q = require('q'),
+    format = require('util').format,
     exec = require('child_process').exec,
     print = require('../../../../lib/helper/print'),
     settings = require('../../../../lib/settings');
 
 module.exports = function (localSettings, config, device, verbose) {
-    var defer = Q.defer();
-    var mode = localSettings.mode ? '-release.apk' : '-debug.apk';
-    var apk_filename = localSettings.configurations['android'][config].product_file_name + mode;
-    var cmd = settings.external.adb.name + ' -s ' + device.value + ' install -rl ' + 'app/platforms/android/ant-build/' + apk_filename;
-    var options = {
-        timeout : 6000,
-        maxBuffer: 1024 * 400
-    };
+    var defer = Q.defer(),
+        mode = localSettings.mode ? '-release.apk' : '-debug.apk',
+        product_name = localSettings.configurations['android'][config].product_file_name,
+        cmd = format(
+            "%s -s %s install -rl app/platforms/android/ant-build/%s",
+            settings.external.adb.name,
+            device.value,
+            product_name + mode
+        ),
+        options = {
+            timeout : 6000,
+            maxBuffer: 1024 * 400
+        };
 
     if(verbose)
         print.success('trying to install android app: %s', apk_filename);
