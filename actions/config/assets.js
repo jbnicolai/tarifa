797f02e3e0df03a101e227712db3af319991d514
Paul Panserrieu <paul.panserrieu@gmail.com>
2014-08-09 13:57:23 +0200

    refactor tarifa config assets

--- actions/config/assets.js
+++ actions/config/assets.js
@@ -2,42 +2,34 @@ var Q = require('q'),
     path = require('path'),
     mkdirp = require('mkdirp'),
     tarifaFile = require('../../lib/tarifa-file'),
+    tarifaPath = require('../../lib/helper/path'),
     settings = require('../../lib/settings'),
     colorHelper = require('../../lib/helper/color'),
     generateIcons = require('../../lib/cordova/icon').generate,
     generateSplashscreens = require('../../lib/cordova/splashscreen').generate,
     createFolders = require('../../lib/cordova/assets').createFolders;
 
-module.exports.generateIcons = function (args, verbose) {
+function generate(args, f, verbose) {
     var cwd = process.cwd(),
         color = args[0],
         config = args[1];
 
     if(!colorHelper.validate(color)) return Q.reject('invalid color!');
 
-    return tarifaFile.parseConfig(path.join(cwd, 'tarifa.json')).then(function (localSettings) {
+    return tarifaFile.parseConfig(tarifaPath.current()).then(function (localSettings) {
         var platforms = localSettings.platforms.filter(function (platform) {
             return platform !== 'web';
         });
         return Q.all(createFolders(cwd, platforms, config)).then(function () {
-            return generateIcons(color, cwd, platforms, config, verbose);
+            return f(color, cwd, platforms, config, verbose);
         });
     });
+}
+
+module.exports.generateIcons = function (args, verbose) {
+    return generate(args, generateIcons, verbose);
 };
 
 module.exports.generateSplashscreens = function (args, verbose) {
-    var cwd = process.cwd(),
-        color = args[0],
-        config = args[1];
-
-    if(!colorHelper.validate(color)) return Q.reject('invalid color!');
-
-    return tarifaFile.parseConfig(path.join(cwd, 'tarifa.json')).then(function (localSettings) {
-        var platforms = localSettings.platforms.filter(function (platform) {
-            return platform !== 'web';
-        });
-        return Q.all(createFolders(cwd, platforms, config, true)).then(function () {
-            return generateSplashscreens(color, cwd, platforms, config, verbose);
-        });
-    });
+    return generate(args, generateSplashscreens, verbose);
 };
