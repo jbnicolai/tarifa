8b24a1faebd7bdf95ee9389e573800997d5d10bc
Paul Panserrieu <paul.panserrieu@gmail.com>
2014-08-09 14:47:14 +0200

    refactor tarifa config

--- actions/config/index.js
+++ actions/config/index.js
@@ -10,53 +10,51 @@ var Q = require('q'),
 var cmds = {
     ios: {
         devices: {
-            list: devices.list,
-            add: devices.add,
-            remove: devices.remove,
-            attach: devices.attach,
-            detach: devices.detach
+            list:   { action: devices.list,   n: [0, 1] },
+            add:    { action: devices.add,    n: [2] },
+            remove: { action: devices.remove, n: [2] },
+            attach: { action: devices.attach, n: [2] },
+            detach: { action: devices.detach, n: [2] }
         },
         provisioning: {
-            fetch: provisioning.fetch,
-            list: provisioning.list
+            fetch: { action: provisioning.fetch, n: [1] },
+            list:  { action: provisioning.list,  n: [0] }
         }
     },
-    icons: assets.generateIcons,
-    splashscreens: assets.generateSplashscreens
+    icons:         { action: assets.generateIcons,         n: [2] },
+    splashscreens: { action: assets.generateSplashscreens, n: [2] }
 };
 
-var config = function (args, verbose) {
-    if (args.length >=3) {
-        var p = args[0],
-            t = args[1],
-            c = args[2],
-            cmd_exists = cmds[p] && cmds[p][t] && cmds[p][t][c];
+function printHelp() {
+    return fs.read(path.join(__dirname, 'usage.txt')).then(print);
+}
 
-        if(cmd_exists) {
-            return cmds[p][t][c](args.splice(2, args.length-1), verbose);
-        }
-        else if(['icons', 'splashscreens'].indexOf(p) > -1) {
-            return cmds[p](args.splice(1, args.length-1), verbose)
-        }
-        else {
-            usage();
-            return Q.resolve();
-        }
-    }
-    usage();
-    return Q.resolve();
+var config = function (args, verbose) {
+    var p = args[0],
+        t = args[1],
+        c = args[2],
+        cmd_exists = cmds[p] && cmds[p][t] && cmds[p][t][c],
+        validArgsCount = cmd_exists && cmds[p][t][c]['n'].indexOf(args.length-3) >-1,
+        isAssetsCmds = ['icons', 'splashscreens'].indexOf(p) > -1;
+
+    if(cmd_exists && validArgsCount)
+        return cmds[p][t][c]['action'](args.splice(2, args.length-1), verbose);
+    else if(isAssetsCmds && args.length === 3)
+        return cmds[p](args.splice(1, args.length-1), verbose)
+    else return printHelp();
 };
 
 var action = function (argv) {
     var verbose = false;
-    if(argsHelper.matchSingleOption(argv, 'h', 'help')) {
-        return fs.read(path.join(__dirname, 'usage.txt')).then(print);
-    }
 
-    if(argsHelper.matchSingleOption(argv, 'V', 'verbose')) verbose = true;
-    return config(argv._, verbose);
+    if(argsHelper.matchArgumentsCount(argv, [3,5])
+            && argsHelper.checkValidOptions(argv, ['V', 'verbose'])) {
+        if(argsHelper.matchOption(argv, 'V', 'verbose'))
+            verbose = true;
+        return config(argv._, verbose);
+    }
+    return printHelp();
 };
 
 action.config = config;
-
 module.exports = action;
