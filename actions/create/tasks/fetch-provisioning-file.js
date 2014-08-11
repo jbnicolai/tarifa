689c2121ea41239d6ce5f8ecc1981e4c6deac2cf
Paul Panserrieu <paul.panserrieu@gmail.com>
2014-08-11 21:32:11 +0200

    oops

--- actions/create/tasks/fetch-provisioning-file.js
+++ actions/create/tasks/fetch-provisioning-file.js
@@ -1,20 +1,20 @@
 var path = require('path'),
     Q = require('q'),
     path = require('path'),
-    downloadProvisioningProfile = require('../../config/ios/provisioning').downloadProvisioningProfile,
+    download = require('../../../lib/ios/nomad/provisioning/download'),
     settings = require('../../../lib/settings');
 
-module.exports = function (response) {
-    if(!response.provisioning_profile_path || !response.provisioning_profile_name) {
-        return Q.resolve(response);
+module.exports = function (r) {
+    if(!r.provisioning_profile_path || !r.provisioning_profile_name) {
+        return Q.resolve(r);
     }
 
-    return downloadProvisioningProfile(
-        response.apple_id,
-        response.apple_developer_team,
-        response.password,
-        response.provisioning_profile_name,
-        path.resolve(response.path, response.provisioning_profile_path),
-        response.options.verbose
-    ).then(function () { return response; });
+    return download(
+        r.apple_id,
+        r.apple_developer_team,
+        r.password,
+        r.provisioning_profile_name,
+        path.resolve(r.path, r.provisioning_profile_path),
+        r.options.verbose
+    ).then(function () { return r; });
 };
