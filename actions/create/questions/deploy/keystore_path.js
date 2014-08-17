76a806c99fd0d2f4bdd3671efbd1d56d81535b75
Baptiste MORELLE <bmo@zengularity.com>
2014-08-17 18:34:40 +0200

    #14 validate the keystore_path and keystore file format

--- actions/create/questions/deploy/keystore_path.js
+++ actions/create/questions/deploy/keystore_path.js
@@ -1,13 +1,18 @@
 var path = require('path'),
-    fs = require('fs');
+    fs = require('fs'),
+    ks = require('../../../../lib/android/keystore');
 
 module.exports = {
     dependency: 'android',
     type:'input',
     name:'keystore_path',
-    validate : function (answer) {
-        // TODO  we need to check if it's really a keystore and not just a file?
-        return fs.existsSync(path.resolve(answer)) || 'file not found!';
+    validate: function (answer) {
+        var done = this.async();
+        ks.check(path.resolve(answer)).then(function () {
+            done(true);
+        }, function (reason) {
+            done(reason);
+        });
     },
     message:'What is the keystore path?'
 };
