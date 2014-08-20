737f785c2d0aee7b98ebcd5dd74ffff72865c55c
Vincent Velociter <vincent.velociter@gmail.com>
2014-08-20 15:35:04 +0200

    First working version of hockeyapp upload

--- actions/build/index.js
+++ actions/build/index.js
@@ -3,6 +3,7 @@ var Q = require('q'),
     argsHelper = require('../../lib/helper/args'),
     print = require('../../lib/helper/print'),
     tarifaPath = require('../../lib/helper/path'),
+    setMode = require('../../lib/helper/setReleaseMode'),
     settings = require('../../lib/settings'),
     tarifaFile = require('../../lib/tarifa-file'),
     path = require('path'),
@@ -129,28 +130,7 @@ var runReleaseTasks = function (type) {
         if (conf.localSettings.mode == '--release')
             return runTasks(type)(conf);
         else return Q.resolve(conf);
-    }
-};
-
-var setMode = function (platform, config, localSettings) {
-    var mode = null,
-        localConf = localSettings.configurations[platform][config];
-
-    if (platform === 'android'
-            && localConf.keystore_path
-            && localConf.keystore_alias) {
-        mode = '--release';
-    }
-    if(platform === 'ios'
-            && localConf.apple_developer_identity
-            && localConf.provisioning_profile_name) {
-        mode = '--release'
-    }
-    if(platform === 'wp8' && localConf.release_mode) {
-        mode = '--release'
-    }
-
-    return mode;
+    };
 };
 
 var buildƒ = function (conf){
--- actions/deploy/index.js
+++ actions/deploy/index.js
@@ -1,39 +1,46 @@
 var Q = require('q'),
     fs = require('q-io/fs'),
     path = require('path'),
+    tarifaFile = require('../../lib/tarifa-file'),
+    pathHelper = require('../../lib/helper/path'),
+    setMode = require('../../lib/helper/setReleaseMode'),
     argsHelper = require('../../lib/helper/args'),
-    devices = require('./ios/devices'),
-    assets = require('./assets'),
-    print = require('../../lib/helper/print'),
-    provisioning = require('./ios/provisioning');
+    hockeyapp = require('../../lib/hockeyapp'),
+    print = require('../../lib/helper/print');
 
 var deployƒ = function (conf) {
-    // TODO
+    conf.localSettings.mode = setMode(conf.platform, conf.configuration, conf.localSettings);
+    var productFileName = pathHelper.productFile(
+        conf.platform,
+        conf.envSettings.product_file_name,
+        conf.localSettings.mode
+    );
+    // TODO refactor to have providers
+    return hockeyapp.uploadVersion(productFileName, conf);
 };
 
 var deploy = function (platform, verbose) {
     var cwd = process.cwd();
     var tarifaFilePath = path.join(cwd, 'tarifa.json');
 
-    // for now we impose 'staging' env as deploy env... this can change
-    var config = 'staging';
+    // for now we impose 'stage' env as deploy env... this can change
+    var config = 'stage';
 
-    return tarifaFile.parseConfig(tarifaPath.current(), platform, config).then(function (localSettings) {
-        if (!localSettings.hockeyapp_id)
-            return Q.reject('No hockeyapp_id key is available in staging for current platform');
+    return tarifaFile.parseConfig(pathHelper.current(), platform, config).then(function (localSettings) {
+        if (!localSettings.configurations[platform][config].hockeyapp_id)
+            return Q.reject('No hockeyapp_id key is available in stage for current platform');
 
-        return tarifaFile.parseConfig(tarifaPath.current()).then(function (globalSettings) {
-            if(!globalSettings.deploy || !globalSettings.deploy.hockeyapp_user || !globalSettings.deploy.hockeyapp_token)
-                return Q.reject("No deploy informations are available in the current tarifa.json file for the chooses provider (hockeyapp)");
+        if (!localSettings.deploy || !localSettings.deploy.hockeyapp_apiurl
+            || !localSettings.deploy.hockeyapp_token)
+            return Q.reject('No deploy informations are available in the current tarifa.json' +
+            'file for the choosen provider (hockeyapp)');
 
-            return deployƒ({
-                hockeyappUser: globalSettings.deploy.hockeyapp_user,
-                hockeyappToken: globalSettings.deploy.hockeyapp_token,
-                platform: platform,
-                configuration: config,
-                localSettings: localSettings,
-                verbose: verbose
-            });
+        return deployƒ({
+            platform: platform,
+            configuration: config,
+            localSettings: localSettings,
+            envSettings: localSettings.configurations[platform][config],
+            verbose: verbose
         });
     });
 };
