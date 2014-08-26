48836e1b1428c9d30b4bef88d933736ab956ffbd
Paul Panserrieu <paul.panserrieu@gmail.com>
2014-08-26 18:02:25 +0200

    working tarifa for release and local run

--- actions/build/tasks/windows8/change_appxmanifest.js
+++ actions/build/tasks/windows8/change_appxmanifest.js
@@ -5,15 +5,22 @@ var Q = require('q'),
 
 module.exports = function (msg) {
     var cwd = process.cwd();
+    var conf = msg.localSettings.configurations.windows8;
     var manifestPath = path.join(cwd, settings.cordovaAppPath, 'platforms', 'windows8', 'package.appxmanifest');
-    var name = msg.localSettings.configurations.windows8[msg.configuration]['product_name'];
+    var name = conf[msg.configuration]['product_name'];
+    var package_id = conf[msg.configuration]['package_id'] || conf[msg.configuration]['id'];
+    var publisher = conf[msg.configuration]['publisher'] || "CN=$username$";
 
+    var publisherAttr = "Publisher=\"" + publisher + "\""; 
     var displayName = "<DisplayName>" + name + "</DisplayName>";
+    var identity = "Identity Name=\"" + package_id + "\"";
     var displayName2 = "DisplayName=\"" + name + "\"";
     var description = "Description=\"" + msg.localSettings.description + "\"";
 
     return fs.read(manifestPath).then(function (manifestContent) {
         var content = manifestContent.replace(/<DisplayName>.*<\/DisplayName>/, displayName)
+            .replace(/Publisher=\"([^\"]*)\"/, publisherAttr)
+            .replace(/Identity Name=\"([^\"]*)\"/, identity)
             .replace(/DisplayName=\"([^\"]*)\"/, displayName2)
             .replace(/Description=\"([^\"]*)\"/, description);
 
--- actions/build/tasks/windows8/rename_app_file.js
+++ actions/build/tasks/windows8/rename_app_file.js
@@ -1,19 +1,28 @@
 var Q = require('q'),
     path = require('path'),
+    ncp = require('ncp').ncp,
     fs = require('q-io/fs'),
     format = require('util').format,
     print = require('../../../../lib/helper/print'),
     settings = require('../../../../lib/settings');
 
-function rename(src, dest) {
-    return function () { fs.rename(src, dest); };
+function copy(src, dest) {
+    return function () {
+        var defer = Q.defer();
+        ncp.limit = 1024;
+        ncp(src, dest, function (err) {
+            if (err) return defer.reject(err);
+            defer.resolve();
+        });
+        return defer.promise;
+    };
 }
 
 module.exports = function (msg) {
     var localeConf = msg.localSettings.configurations.windows8,
-        filename = msg.localSettings.configurations.windows8[msg.configuration].product_file_name,
+        filename = localeConf[msg.configuration].product_file_name,
         release = msg.localSettings.mode === '--release',
-        version = (msg.localSettings.configurations.windows8[msg.configuration].version || msg.localSettings.version) + '.0',
+        version = (localeConf[msg.configuration].version || msg.localSettings.version) + '.0',
         defaultFilename = format("%s_%s_%s_%s", 'CordovaApp', version, 'AnyCPU', release ? 'Test': 'Debug_Test'),
         defaultFilenameAppx = format("%s_%s_%s%s", 'CordovaApp', version, 'AnyCPU', release ? '': '_Debug'),
         newFilename = format("%s.%s.%s", filename, msg.configuration, version),
@@ -24,9 +33,9 @@ module.exports = function (msg) {
         target2 = path.join(dir, newFilename + '.appxupload');
 
     return Q.all([fs.remove(target1), fs.remove(target2)])
-        .then(rename(src1, target1), rename(src1, target1))
-        .then(function () { if(msg.verbose) print.success('rename generated app'); })
-        .then(rename(src2, target2), rename(src2, target2))
-        .then(function () { if(msg.verbose) print.success('rename generated .appxupload generated file'); })
+        .then(copy(src1, target1), copy(src1, target1))
+        .then(function () { if(msg.verbose) print.success('copy generated app'); })
+        .then(copy(src2, target2), copy(src2, target2))
+        .then(function () { if(msg.verbose) print.success('copy generated .appxupload generated file'); })
         .then(function () { return msg; });
 };
