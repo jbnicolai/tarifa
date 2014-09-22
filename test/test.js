4b40463523eee0961b525163f2b2a9b6002b5405
Paul Panserrieu <paul.panserrieu@gmail.com>
2014-09-22 12:17:54 +0200

    bootstrap tests: first tests xml files modifications

---
+++ test/test.js
@@ -0,0 +1,5 @@
+var should = require('should');
+
+describe('tarifa', function(){
+    it('todo');
+});
---
+++ test/xml/android/AndroitManifest.xml.js
@@ -0,0 +1,7 @@
+var should = require('should');
+
+describe('replacing stuff in AndroidManifest.xml', function(){
+    describe('be able to read the versionCode attriute', function(){
+        it('todo');
+    });
+});
