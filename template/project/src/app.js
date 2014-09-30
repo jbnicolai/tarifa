4a0b24b312d9796ce041ad36bc3ac270e9e8170b
Paul Panserrieu <paul.panserrieu@gmail.com>
2014-09-30 22:04:13 +0200

    fix #10 better www project template

--- template/project/src/app.js
+++ template/project/src/app.js
@@ -1,11 +1,35 @@
 var qstart = require('qstart'),
+    Q = require('Q'),
+    format = require('util').format,
     settings = require('settings'),
     Zanimo = require('zanimo');
 
 qstart.then(function () {
-    var p = window.document.querySelector('p');
-    Zanimo(p, 'transform', 'scale(0.5)', 500, 'ease-in-out').then(function (el) {
-        el.innerHTML = 'welcome to ' + settings.appName + ' with id ' + settings.bundleId + ' with version: ' + settings.version;
-        return el;
-    }).then(Zanimo.f('transform', 'scale(1)', 500, 'ease-in-out'));
+    var p = window.document.querySelector('p.info'),
+        defer = Q.defer(),
+        logo = window.document.querySelector('.main.circle');
+
+    if(navigator.splashscreen) {
+        setTimeout(function () {
+            navigator.splashscreen.hide();
+            setTimeout(function () {
+                defer.resolve();
+            }, 2000);
+        }, 1000);
+    }
+    else {
+        defer.resolve();
+    }
+
+    defer.promise.then(function () {
+        return Zanimo(logo, 'transform', 'rotateZ(0deg)', 300, 'ease-in-out');
+    }).then(function () {
+        p.innerHTML = format(
+            '<dl><dt>name</dt><dd>%s</dd><dt>id</dt><dd>%s</dd><dt>version</dt><dd>%s</dd></dl>',
+            settings.appName,
+            settings.bundleId,
+            settings.version
+        );
+        return p;
+    }).then(Zanimo.f('display', 'block')).then(Zanimo.f('opacity', 1, 400));
 });
