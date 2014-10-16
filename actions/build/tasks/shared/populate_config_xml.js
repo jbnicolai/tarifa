30e7f1b2d9fa1d23cdd26bfcf92f923fbd525860
Vincent Velociter <vincent.velociter@gmail.com>
2014-10-16 19:14:29 +0200

    Allow accessOrigin override
    Ref #110

--- actions/build/tasks/shared/populate_config_xml.js
+++ actions/build/tasks/shared/populate_config_xml.js
@@ -5,14 +5,15 @@ var Q = require('q'),
     ConfigBuilder = require('../../../../lib/xml/config.xml');
 
 module.exports = function (msg) {
-    var id = msg.localSettings.configurations[msg.platform][msg.configuration]['id'];
+    var conf = msg.localSettings.configurations[msg.platform][msg.configuration],
+        id = conf.id,
         author = msg.localSettings.author.name,
         author_email = msg.localSettings.author.email,
         author_href = msg.localSettings.author.href,
         description = msg.localSettings.description,
-        version = msg.localSettings.configurations[msg.platform][msg.configuration]['version'] || msg.localSettings.version,
+        version = conf.version || msg.localSettings.version,
         preferences = msg.localSettings.cordova.preferences,
-        accessOrigin = msg.localSettings.cordova.accessOrigin,
+        accessOrigin = conf.cordova_accessOrigin || msg.localSettings.cordova.accessOrigin,
         config_xml_path = path.join(process.cwd(), settings.cordovaAppPath, 'config.xml');
 
     return ConfigBuilder.set(config_xml_path, id, version, author, author_email, author_href, description, preferences, accessOrigin).then(function () {
