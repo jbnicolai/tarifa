8fe12e91b6f4ca7181032842ae98e76337c90cd4
Paul Panserrieu <paul.panserrieu@gmail.com>
2014-10-23 09:46:42 +0200

    fix #119 tarifa run on windows and linux

--- actions/run/tasks/browser/open.js
+++ actions/run/tasks/browser/open.js
@@ -1,12 +1,13 @@
 var Q = require('q'),
     format = require('util').format,
     exec = require('child_process').exec,
+    spawn = require('child_process').spawn,
     path = require('path'),
     print = require('../../../../lib/helper/print'),
     pathHelper = require('../../../../lib/helper/path'),
     settings = require('../../../../lib/settings');
 
-module.exports = function (conf) {
+function openChromeOnDarwin(conf) {
     var defer = Q.defer(),
         cmd = path.join('platforms', 'browser', 'cordova', 'run'),
         options = {
@@ -14,8 +15,6 @@ module.exports = function (conf) {
             timeout : 100000,
             maxBuffer: 1024 * 400
         };
-
-    if(process.platform === 'linux') return Q.reject("Can't run browser platform in linux!");
 
     if(conf.verbose)
         print.success('trying to open browser!');
@@ -34,4 +33,43 @@ module.exports = function (conf) {
     });
 
     return defer.promise;
+}
+
+function openChromeOnWin32(conf) {
+    var indexPath = path.resolve(process.cwd(), settings.cordovaAppPath, 'platforms', 'browser', 'www', 'index.html'),
+        project = format('file://%s', indexPath);
+        child = spawn(
+            'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe',
+            ['--user-data-dir=C:/Chromedevsession', '--disable-web-security', project],
+            { detached: true , stdio:'ignore'}
+        );
+
+    child.unref();
+    return Q.resolve(conf);
+}
+
+function openChromeOnLinux(conf) {
+    var indexPath = path.resolve(process.cwd(), settings.cordovaAppPath, 'platforms', 'browser', 'www', 'index.html'),
+        project = format('file://%s', indexPath);
+        child = spawn(
+            'chromium',
+            ['--user-data-dir=/tmp/temp_chrome_user_data_dir_for_cordova_browser', '--disable-web-security', project],
+            { detached: true , stdio:'ignore'}
+        );
+
+    child.unref();
+    return Q.resolve(conf);
+}
+
+module.exports = function (conf) {
+    switch(process.platform) {
+        case 'darwin':
+            return openChromeOnDarwin(conf);
+        case 'win32':
+            return openChromeOnWin32(conf);
+        case 'linux':
+            return openChromeOnLinux(conf);
+        default:
+            return Q.reject(format('Can not run on platform %s!', process.platform));
+    }
 };
