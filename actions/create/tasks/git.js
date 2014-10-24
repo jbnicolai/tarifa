0bd734637a79e6a31f1dd1ac84b36230dc9658af
Baptiste Morelle <bapmrl@gmail.com>
2014-10-24 14:17:55 +0200

    formatting

--- actions/create/tasks/git.js
+++ actions/create/tasks/git.js
@@ -18,12 +18,12 @@ function createGitIgnoreFiles(response) {
     ];
     var promises = gitIgnoreBuilders.map(function (builder) {
         var create = function () {
-                var dest = path.join(builder.destdir, '.gitignore'),
-                    p = Q.resolve();
-                if (builder.src) p = p.then(function () { return fs.copy(builder.src, dest); });
-                if (builder.content) p = p.then(function () { return fs.append(dest, builder.content); });
-                return p;
-            };
+            var dest = path.join(builder.destdir, '.gitignore'),
+                p = Q.resolve();
+            if (builder.src) p = p.then(function () { return fs.copy(builder.src, dest); });
+            if (builder.content) p = p.then(function () { return fs.append(dest, builder.content); });
+            return p;
+        };
         return fs.isDirectory(builder.destdir).then(function (exists) {
             return exists ? create() : Q.resolve();
         });
