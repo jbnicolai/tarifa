aae89afa67589e05e4c4814a7858b48948a826f0
Paul Panserrieu <paul.panserrieu@gmail.com>
2014-11-24 14:04:38 +0100

    allowing ~ on keystore path answer for tarifa create

--- actions/create/questions/deploy/keystore_path_existing.js
+++ actions/create/questions/deploy/keystore_path_existing.js
@@ -1,4 +1,4 @@
-var path = require('path'),
+var pathHelper = require('../../../../lib/helper/path'),
     ks = require('../../../../lib/android/keystore'),
     Configstore = require('configstore'),
     conf = new Configstore('tarifa');
@@ -9,14 +9,14 @@ module.exports = {
     name: 'keystore_path',
     validate: function (answer) {
         var done = this.async();
-        ks.check(path.resolve(answer)).then(function () {
+        ks.check(pathHelper.resolve(answer)).then(function () {
             done(true);
         }, function (reason) {
             done(reason);
         });
     },
     filter: function (answer) {
-        return path.resolve(answer);
+        return pathHelper.resolve(answer);
     },
     message: 'What is the keystore path?',
     default: conf.get('keystore_path')
