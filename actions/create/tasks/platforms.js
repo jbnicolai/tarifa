fd25ea53d91577413493cb8d7d3e682350e56e6a
Paul Panserrieu <paul.panserrieu@gmail.com>
2015-01-05 15:45:27 +0100

    [iOS] copy build.xcconfig file on `tarifa platform add` fix #153

--- actions/create/tasks/platforms.js
+++ actions/create/tasks/platforms.js
@@ -11,13 +11,10 @@ var Q = require('q'),
 
 module.exports = function (response) {
     if (!response.platforms.length) return response;
-    return platformsLib.add(pathHelper.resolve(response.path), response.platforms, response.options.verbose).then(function() {
-        if (response.platforms.indexOf('ios') > -1) {
-            var src = path.join(__dirname, 'build.xcconfig'),
-                dest = path.join(pathHelper.resolve(response.path), settings.cordovaAppPath, 'platforms', 'ios', 'cordova', 'build.xcconfig');
-            return fs.copy(src, dest).then(function () { return response; });
-        }else {
-            return response;
-        }
-    });
+
+    return platformsLib.add(
+        pathHelper.resolve(response.path),
+        response.platforms,
+        response.options.verbose
+    ).then(function() { return response; });
 };
