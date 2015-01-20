b7b13c9fb389060d5faa059c63dfff76aca0fef9
Paul Panserrieu <paul.panserrieu@gmail.com>
2015-01-20 15:34:46 +0100

    extract platform specific stuff from `tarifa config`

--- actions/config/index.js
+++ actions/config/index.js
@@ -4,10 +4,26 @@ var Q = require('q'),
     argsHelper = require('../../lib/helper/args'),
     pathHelper = require('../../lib/helper/path'),
     print = require('../../lib/helper/print'),
-    match = require('../../lib/helper/args').matchCmd;
+    match = require('../../lib/helper/args').matchCmd,
+    settings = require('../../lib/settings'),
+    platformCommands = [ ];
 
-function printHelp() {
-    return fs.read(path.join(__dirname, 'usage.txt')).then(print);
+settings.platforms.forEach(function (p) {
+    var mod = path.resolve(__dirname, '../../lib/platforms', p, 'actions/config');
+    platformCommands = platformCommands.concat(require(mod).commands);
+});
+
+function usage() {
+    return fs.read(path.join(__dirname, 'usage.txt')).then(function (msg) {
+        return settings.platforms.reduce(function (text, platform) {
+            var usagePath = path.resolve(__dirname, '../../lib/platforms', platform, 'actions/config/usage.txt');
+            return Q.when(text, function (t) {
+                return fs.read(usagePath).then(function (u) {
+                    return t + u;
+                });
+            });
+        }, msg);
+    }).then(print);
 }
 
 var action = function (argv) {
@@ -17,22 +33,17 @@ var action = function (argv) {
         if(argsHelper.matchOption(argv, 'V', 'verbose'))
             verbose = true;
 
-        if(match(argv._, ['ios', 'devices', 'list', '*'])) return require('./ios/devices').list(argv._[3], verbose);
-        if(match(argv._, ['ios', 'devices', 'add', '+', '+'])) return require('./ios/devices').add(argv._[3], argv._[4], verbose);
-        if(match(argv._, ['ios', 'devices', 'attach', '+', '+'])) return require('./ios/devices').attach(argv._[3], argv._[4], verbose);
-        if(match(argv._, ['ios', 'devices', 'detach', '+', '+'])) return require('./ios/devices').detach(argv._[3], argv._[4], verbose);
-
-        if(match(argv._, ['provisioning', 'list'])) return require('./ios/provisioning').list(verbose);
-        if(match(argv._, ['provisioning', 'fetch', '*'])) return require('./ios/provisioning').fetch(verbose);
-        if(match(argv._, ['provisioning', 'info', '*'])) return require('./ios/provisioning').info(argv._[2], verbose);
-
         if(match(argv._, ['icons', 'generate', '+', '*'])) return action.generateIcons(argv._[2], argv._[3], verbose);
         if(match(argv._, ['icons', 'file', '+', '*'])) return action.generateIconsFromFile(pathHelper.resolve(argv._[2]), argv._[3], verbose);
         if(match(argv._, ['splashscreens', '+', '*'])) return action.generateSplashscreens(argv._[1], argv._[2], verbose);
 
-        return printHelp();
+        for(var i=0,l=platformCommands.length; i<l; i++) {
+            if(match(argv._, platformCommands[i].def))
+                return platformCommands[i].action(argv._, verbose);
+        }
+        return usage();
     }
-    return printHelp();
+    return usage();
 };
 
 action.generateIcons = function (color, config, verbose) {
