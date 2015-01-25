1c43896a020489e127532a4fb0e371c4a5b874f1
Paul Panserrieu <paul.panserrieu@gmail.com>
2015-01-25 20:06:15 +0100

    refactor `tarifa run`

--- actions/run/index.js
+++ actions/run/index.js
@@ -4,6 +4,7 @@ var Q = require('q'),
     exec = require('child_process').exec,
     path = require('path'),
     fs = require('q-io/fs'),
+    intersection = require("interset/intersection"),
     argsHelper = require('../../lib/helper/args'),
     print = require('../../lib/helper/print'),
     settings = require('../../lib/settings'),
@@ -34,13 +35,11 @@ var runƒ = function (conf) {
         });
 };
 
-var run = function (platform, config, cleanResources, verbose) {
+var run = function (platform, config, localSettings, cleanResources, verbose) {
     print.outline('Launch run for %s platform and configuration %s !', platform, config);
     spinner();
-    return Q.all([
-            tarifaFile.parse(pathHelper.root(), platform, config),
-            platformsLib.isAvailableOnHost(platform)
-        ]).spread(function (localSettings) {
+    return platformsLib.isAvailableOnHost(platform)
+        .then(function () {
             return runƒ({
                 localSettings: localSettings,
                 platform: platform,
@@ -48,34 +47,32 @@ var run = function (platform, config, cleanResources, verbose) {
                 cleanResources: cleanResources,
                 verbose: verbose
             });
-        });
+    });
 };
 
-var runMultipleConfs = function(platform, configs, cleanResources, verbose) {
-    return tarifaFile.parse(pathHelper.root(), platform).then(function (localSettings) {
-        configs = configs || tarifaFile.getPlatformConfigs(localSettings, platform);
-        return tarifaFile.checkConfigurations(configs, platform, localSettings).then(function () {
-            return configs.reduce(function(promise, conf) {
-                return promise.then(function () {
-                    return run(platform, conf, cleanResources, verbose);
-                });
-            }, Q());
-        });
+var runMultipleConfs = function(platform, configs, localSettings, cleanResources, verbose) {
+    configs = configs || tarifaFile.getPlatformConfigs(localSettings, platform);
+    return tarifaFile.checkConfigurations(configs, platform, localSettings).then(function () {
+        return configs.reduce(function(promise, conf) {
+            return promise.then(function () {
+                return run(platform, conf, localSettings, cleanResources, verbose);
+            });
+        }, Q());
     });
 };
 
 var runMultiplePlatforms = function (platforms, config, cleanResources, verbose) {
     return tarifaFile.parse(pathHelper.root()).then(function (localSettings) {
         platforms = platforms || localSettings.platforms;
-        return tarifaFile.checkPlatforms(platforms, settings.platforms).then(function () {
-            return platforms.filter(platformsLib.isAvailableOnHostSync).reduce(function(promise, platform) {
+        return tarifaFile.checkPlatforms(platforms, localSettings).then(function (availablePlatforms) {
+            return intersection(platforms, availablePlatforms).reduce(function(promise, platform) {
                 return promise.then(function () {
                     if (config === 'all') {
                         config = null;
                     } else if (argsHelper.matchWildcard(config)) {
                         config = argsHelper.getFromWildcard(config);
                     }
-                    return runMultipleConfs(platform, config, cleanResources, verbose);
+                    return runMultipleConfs(platform, config, localSettings, cleanResources, verbose);
                 });
             }, Q());
         });
@@ -87,9 +84,8 @@ var action = function (argv) {
     cleanResources = false,
     helpPath = path.join(__dirname, 'usage.txt');
 
-    if(argsHelper.matchOption(argv, 'V', 'verbose')) {
+    if(argsHelper.matchOption(argv, 'V', 'verbose'))
         verbose = true;
-    }
 
     if (argsHelper.matchOption(argv, null, 'clean-resources'))
         cleanResources = true;
@@ -99,15 +95,6 @@ var action = function (argv) {
 
     if (argsHelper.matchCmd(argv._, ['__some__', '*']))
         return runMultiplePlatforms(argsHelper.getFromWildcard(argv._[0]), argv._[1] || 'default', cleanResources, verbose);
-
-    if (argsHelper.matchCmd(argv._, ['+', '__all__']))
-        return runMultipleConfs(argv._[0], null, cleanResources, verbose);
-
-    if (argsHelper.matchCmd(argv._, ['+', '__some']))
-        return runMultipleConfs(argv._[0], argsHelper.getFromWildcard(argv._[1], cleanResources, verbose));
-
-    if(argsHelper.matchCmd(argv._, ['+', '*']))
-        return run(argv._[1], argv._[0] || 'default', cleanResources, verbose);
 
     return fs.read(helpPath).then(print);
 };
