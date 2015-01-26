871045d80e10e1b532141f722d229e20a34685ed
Paul Panserrieu <paul.panserrieu@gmail.com>
2015-01-26 22:26:32 +0100

    mv platform specific code from `tarifa create plugin` to lib/platforms/

--- actions/create/plugin/index.js
+++ actions/create/plugin/index.js
@@ -8,6 +8,7 @@ var Q = require('q'),
     PluginBuilder = require('../../../lib/xml/plugin.xml'),
     pathHelper = require('../../../lib/helper/path'),
     print = require('../../../lib/helper/print'),
+    settings = require('../../../lib/settings'),
 
     questions = [
         'plugin/path',
@@ -19,11 +20,30 @@ var Q = require('q'),
         'plugin/author_name',
         'plugin/keywords',
         'plugin/license'
-    ];
+    ],
+
+    templates = {
+        www: {
+            root: path.resolve(__dirname, 'template'),
+            files: [ 'www/$NAME.js' ]
+        }
+    },
+
+    root = path.resolve(__dirname, '../../../');
+
+settings.platforms.forEach(function (platform) {
+    templates[platform] = require(path.resolve(
+            root,
+            'lib/platforms',
+            platform,
+            'actions/create/plugin'
+    ));
+});
 
 function create(verbose) {
     if (verbose) print.banner();
-    return ask(questions)({ options: { verbose: verbose } }).then(function (resp) {
+    var opts = { options: { verbose: verbose } };
+    return ask(questions)(opts).then(function (resp) {
         print();
         spinner();
         return launchTasks(resp);
@@ -47,7 +67,20 @@ function makeRootDirectory(resp) {
 function copyPluginXml(resp) {
     var tmplPath = path.join(__dirname, 'template', 'plugin.xml'),
         destPath = path.join(resp.path, 'plugin.xml');
+
     return fs.read(tmplPath).then(function (tmplContent) {
+        return settings.platforms.reduce(function (xmlP, platform) {
+            return Q.when(xmlP, function (xml) {
+                var pluginXMLPath = templates[platform].pluginXMLPath;
+                return fs.read(pluginXMLPath).then(function (content) {
+                    xml += content; return xml;
+                });
+            });
+        }, '').then(function (platformsContent) {
+            return tmplContent.replace(/\$PLATFORMS/g, platformsContent);
+        });
+
+    }).then(function (tmplContent) {
         var destContent = tmplContent.replace(/\$ID/g, resp.id)
                                      .replace(/\$TARGET_DIR/g, resp.id.replace('.', '/'))
                                      .replace(/\$NAME/g, resp.name)
@@ -58,21 +91,22 @@ function copyPluginXml(resp) {
                                      .replace(/\$LICENSE/g, resp.license);
         return fs.write(destPath, destContent);
     }).then(function () {
-        var platformsToRemove = difference(Object.keys(platformToFiles), resp.platforms.concat('www'));
+        var platformsToRemove = difference(Object.keys(templates), resp.platforms.concat('www'));
         return PluginBuilder.removePlatforms(destPath, platformsToRemove);
     }).then(function () { return resp; });
 }
 
 function copyPlatformsFiles(resp) {
-    var platformsToCopy = intersection(Object.keys(platformToFiles), resp.platforms.concat('www')),
-        filesToCopy = Array.prototype.concat.apply([], platformsToCopy.map(function (platform) {
-            return platformToFiles[platform];
+    var platforms = intersection(Object.keys(templates), resp.platforms.concat('www')),
+        filesToCopy = Array.prototype.concat.apply([], platforms.map(function (platform) {
+            return templates[platform].files.map(function (f) {
+                return { src: path.resolve(templates[platform].root, f), dest: f };
+            });
         }));
-    return filesToCopy.reduce(function (promise, srcPath) {
+    return filesToCopy.reduce(function (promise, file) {
         return promise.then(function () {
-            var tmplPath = path.join(__dirname, 'template', srcPath);
-            return fs.read(tmplPath).then(function (tmplContent) {
-                var destPath = path.join(resp.path, srcPath.replace(/\$NAME/g, resp.name)),
+            return fs.read(file.src).then(function (tmplContent) {
+                var destPath = path.resolve(resp.path, file.dest.replace(/\$NAME/g, resp.name)),
                     destContent = tmplContent.replace(/\$ID/g, resp.id)
                                              .replace(/\$NAME/g, resp.name);
                 return fs.makeTree(path.dirname(destPath)).then(function () {
@@ -80,16 +114,8 @@ function copyPlatformsFiles(resp) {
                 });
             });
         });
-    }, Q.resolve()).then(function () { return resp; });
+    }, Q()).then(function () { return resp; });
 }
-
-var platformToFiles = {
-    android: [ path.join('src', 'android', '$NAME.java') ],
-    browser: [ path.join('src', 'browser', '$NAME.js') ],
-    ios: [ path.join('src', 'ios', '$NAME.h'), path.join('src', 'ios', '$NAME.m') ],
-    wp8: [ path.join('src', 'wp', '$NAME.cs') ],
-    www: [ path.join('www', '$NAME.js') ]
-};
 
 create.launchTasks = launchTasks;
 module.exports = create;
