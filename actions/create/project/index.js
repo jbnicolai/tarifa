b52e6e1d8a6567f046c3e200c72cab1ed3c4105e
Paul Panserrieu <paul.panserrieu@gmail.com>
2015-02-01 12:48:43 +0100

    mv project tasks into platform folder

--- actions/create/project/index.js
+++ actions/create/project/index.js
@@ -1,29 +1,17 @@
 var Q = require('q'),
     spinner = require('char-spinner'),
     ask = require('../../../lib/questions/ask'),
-    questions = require('../../../lib/questions/list').project,
-    print = require('../../../lib/helper/print'),
-
-    tasks = [
-        './tasks/tarifa',
-        './tasks/cordova',
-        './tasks/platforms',
-        './tasks/fetch-provisioning-file',
-        './tasks/create-keystore',
-        './tasks/tarifa-file',
-        './tasks/git',
-        './tasks/plugins',
-        './tasks/assets'
-    ];
+    questions = require('../../../lib/questions/list'),
+    print = require('../../../lib/helper/print');
 
 function launchTasks(resp) {
-    return tasks.map(require).reduce(Q.when, resp);
+    return questions.projectTasks.map(require).reduce(Q.when, resp);
 }
 
 function create(verbose) {
     if (verbose) print.banner();
     var response = { options : { verbose : verbose } };
-    return ask(questions)(response).then(function (resp) {
+    return ask(questions.project)(response).then(function (resp) {
         print(); spinner();
         return launchTasks(resp);
     });
