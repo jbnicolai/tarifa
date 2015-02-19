9375725ca7d6b41f7f6a808da7c45ace312acbb3
Paul Panserrieu <paul.panserrieu@gmail.com>
2015-02-19 17:33:25 +0100

    more tarifa check tests

--- test/actions/check.js
+++ test/actions/check.js
@@ -3,15 +3,32 @@ var should = require('should'),
     os = require('os'),
     format = require('util').format,
     tmp = require('tmp'),
+    rimraf = require('rimraf'),
+    path = require('path'),
+    settings = require('../../lib/settings'),
     setupHelper = require('../helper/setup'),
     checkAction = require('../../actions/check');
 
 function testCheck(projectDefer) {
     describe('tarifa check', function() {
-        it('should not fail', function () {
+        it('should not fail with a working project', function () {
             this.timeout(0);
             return projectDefer.promise.then(function (rslt) {
                 return checkAction.check(false);
+            });
+        });
+        it('should not fail after removing the `app` and `images` folder', function () {
+            this.timeout(0);
+            return projectDefer.promise.then(function (rslt) {
+                rimraf.sync(path.resolve(rslt.dirPath, settings.cordovaAppPath));
+                rimraf.sync(path.resolve(rslt.dirPath, settings.images));
+                return checkAction.check(false);
+            });
+        });
+        it('should not fail when used with --force', function () {
+            this.timeout(0);
+            return projectDefer.promise.then(function (rslt) {
+                return checkAction.check(true);
             });
         });
     });
